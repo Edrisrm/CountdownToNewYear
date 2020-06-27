0c16b251413e8b42ad231291234074feac7e1281
Edrisrm <44209633+Edrisrm@users.noreply.github.com>
2020-06-27 11:16:40 -0600

    Mejora de cronometro

--- app/src/main/java/com/example/countdowntonewyear/MainActivity.java
+++ app/src/main/java/com/example/countdowntonewyear/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.countdowntonewyear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -10,20 +12,22 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import cn.iwgang.countdownview.CountdownView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView tv;
+    long diff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tv = findViewById(R.id.cronometro);
         CountdownView mCvCountdownView = findViewById(R.id.mycountdown);
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        String countDate = "01-01-2020 00:00:00";
+        String countDate = "27-06-2020 03:28:00";
         Date now = new Date();
 
 
@@ -32,13 +36,37 @@ public class MainActivity extends AppCompatActivity {
             Date date = sdf.parse(countDate);
             long currentTime = now.getTime();
             long newYearDate = date.getTime();
-            long countDownToNewYear = newYearDate - currentTime;
-            mCvCountdownView.start(countDownToNewYear);
+             diff = newYearDate - currentTime;
+            mCvCountdownView.start(diff);
 
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        MyCount counter = new MyCount(diff, 1000);
+        counter.start();
 
+    }
+    public class MyCount extends CountDownTimer {
+        public TextView txtNumber1;
+        MyCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
 
+        @Override
+        public void onFinish() {
+
+            tv.setText("done!");
+            tv.setBackgroundColor(Color.RED);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long millis = millisUntilFinished;
+            String hms = (TimeUnit.MILLISECONDS.toDays(millis)) + "Day "
+                    + (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis)) + ":")
+                    + (TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)) + ":"
+                    + (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis))));
+            tv.setText(/*context.getString(R.string.ends_in) + " " +*/ hms);
+        }
     }
 }
